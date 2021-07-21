a9554e8e464028f5152b289c4c9ebdd77d703608
Tomas <tomasoares@gmail.com>
2021-07-21 12:06:14 +0100

    Initial project setup

---
+++ src/main/java/com/synelience/weatherapi/WeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.synelience.weatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synelience/weatherapi/WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synelience.weatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
